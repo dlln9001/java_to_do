51cef44b0bca0ba369b0ef41cabfb5753eebe32b
Dillon <dfcross18@gmail.com>
2025-08-18 21:52:38 -0700

    changed model name, added id primary key

--- backend/task_tracker/task_tracker/src/main/java/com/todo/task_tracker/model/HomeModel.java
+++
@@ -1,5 +0,0 @@
-package com.todo.task_tracker.model;
-
-public class HomeModel {
-    
-}
---
+++ backend/task_tracker/task_tracker/src/main/java/com/todo/task_tracker/model/Task.java
@@ -0,0 +1,14 @@
+package com.todo.task_tracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
