83a28af016c560edbe4abe28fadef765eefff370
Dillon Crossman <dfcross18@gmail.com>
2025-08-19 15:03:01 -0700

    added endpoint for creating task

--- backend/task_tracker/task_tracker/src/main/java/com/todo/task_tracker/controller/TaskController.java
+++ backend/task_tracker/task_tracker/src/main/java/com/todo/task_tracker/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.todo.task_tracker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.todo.task_tracker.service.TaskService;
 import com.todo.task_tracker.model.Task;
@@ -23,6 +25,11 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
+    }
+
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
     }
 }
  
